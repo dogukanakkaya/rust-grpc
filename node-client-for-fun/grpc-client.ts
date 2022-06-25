df8a6574e072bbd7978bc16028d188e4a0b3485b
dogukanakkaya <doguakkaya27@hotmail.com>
2022-06-25 23:00:05 +0300

    feat: grpc server addr from env or default

--- node-client-for-fun/grpc-client.ts
+++ node-client-for-fun/grpc-client.ts
@@ -3,4 +3,6 @@ const protoLoader = require('@grpc/proto-loader');
 const packageDefinition = protoLoader.loadSync('../proto/todo.proto', {});
 const todoPackage = grpc.loadPackageDefinition(packageDefinition).todo;
 
-export const todoClient = new todoPackage.Todo('localhost:8080', grpc.credentials.createInsecure());
+const serverAddr = process.env.GRPC_SERVER_ADDR || '0.0.0.0:8080';
+
+export const todoClient = new todoPackage.Todo(serverAddr, grpc.credentials.createInsecure());
