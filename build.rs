2a97f18f03155dc6d762ce74ab81bce0bd29ac19
dogukanakkaya <doguakkaya27@hotmail.com>
2022-06-26 01:41:29 +0300

    refactor: set build client to true for compilation

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_client(false)
+        // .build_client(false)
         .compile(&["proto/todo.proto"], &["proto"])
         .unwrap();
     Ok(())
