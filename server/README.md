3689b7734fd47919187379da9ed7bfa832fcfe1b
dogukanakkaya <doguakkaya27@hotmail.com>
2022-06-25 19:39:39 +0300

    refactor: server code

--- server/README.md
+++ server/README.md
@@ -1,4 +1,4 @@
 ```
 docker build -t rust-grpc-server .
-docker run -dit --name rust-grpc-server -v $(pwd):/usr/src/app -v /usr/src/app/target  rust-grpc-server
+docker run -dit --name rust-grpc-server -v $(pwd):/usr/src/app -v /usr/src/app/target -p 8080:50051 rust-grpc-server
 ```
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,7 +1,7 @@
 use std::sync::Mutex;
 use tonic::{transport::Server, Request, Response, Status};
 use todo::todo_server::{TodoServer, Todo};
-use todo::{GetTodosResponse, CreateTodoRequest, CreateTodoResponse};
+use todo::{TodoItem, GetTodosResponse, CreateTodoRequest, CreateTodoResponse};
 
 pub mod todo {
     tonic::include_proto!("todo");
@@ -9,7 +9,7 @@ pub mod todo {
 
 #[derive(Debug, Default)]
 pub struct TodoService {
-    todos: Mutex<Vec<todo::TodoItem>>
+    todos: Mutex<Vec<TodoItem>>
 }
 
 #[tonic::async_trait]
@@ -23,22 +23,17 @@ impl Todo for TodoService {
     async fn create_todo(&self, request: Request<CreateTodoRequest>) -> Result<Response<CreateTodoResponse>, Status> {
         let payload = request.into_inner();
 
-        let todo_item = todo::TodoItem {
+        let todo_item = TodoItem {
             name: payload.name,
             description: payload.description,
             priority: payload.priority,
             completed: false
         };
 
-        self.todos.lock().unwrap().push(todo_item);
+        self.todos.lock().unwrap().push(todo_item.clone());
         
         let message = CreateTodoResponse {
-            todo: Some(todo::TodoItem {
-                name: "payload.name".to_string(),
-                description: "payload.description".to_string(),
-                priority: 2,
-                completed: false
-            }),
+            todo: Some(todo_item),
             status: true
         };
 
